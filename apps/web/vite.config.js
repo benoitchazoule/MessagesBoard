c8cb1179a47c111b766269abcd9af9e2aa64d233
Benoit CHAZOULE <benoit@marmelab.com>
2025-11-26 17:14:08 +0100

    forgot doing correct import to use copyFileSync

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { copyFileSync } from "fs";
 
 // Custom plugin to copy _redirects file
 function copyRedirects() {
